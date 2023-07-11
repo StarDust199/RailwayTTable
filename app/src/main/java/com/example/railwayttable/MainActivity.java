8e3591130865487752ea7b8bd4c08ae611493218
ahamal209864 <agnieszka.hamal@gmail.com>
2023-07-11 17:58:34 +0200

    working navigation drawer

--- app/src/main/java/com/example/railwayttable/MainActivity.java
+++ app/src/main/java/com/example/railwayttable/MainActivity.java
@@ -1,14 +1,73 @@
 package com.example.railwayttable;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+    }
+
+    public void openRoute(View v) {
+        Intent intent;
+        intent = new Intent(MainActivity.this, RouteActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+
+    }
+    public void openFavorites(View v) {
+        Intent intent;
+        intent = new Intent(MainActivity.this, FavoritesActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+
+    }
+
+
+    public void openTimetable(View v) {
+        Intent intent;
+        intent = new Intent(MainActivity.this, TimetableActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+
+    }
+    public void openPlanner(View v) {
+        Intent intent;
+        intent = new Intent(MainActivity.this, PlannerActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+
     }
 }
---
+++ app/src/main/java/com/example/railwayttable/RouteActivity.java
@@ -0,0 +1,20 @@
+package com.example.railwayttable;
+
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class RouteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route);
+
+
+    }
+}
